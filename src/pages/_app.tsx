8f5bda35924f66d23b0dd99bbbefb800d5391dd1
Mahesh <maheshghamand@gmail.com>
2023-11-20 17:13:31 +0530

    signup-page: phase 1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
 import "~/styles/globals.css";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        <Component {...pageProps} />
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
 };
 
 export default MyApp;
